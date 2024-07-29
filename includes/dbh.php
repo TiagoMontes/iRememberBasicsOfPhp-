29eca75f07fa57d449f8b0831d68bf9d47b6ea21
TiagoMontes <tiagosantos6303@gmail.com>
2024-07-28 19:06:53 -0500

    about PDO

--- includes/dbh.php
+++ includes/dbh.php
@@ -6,6 +6,10 @@ $username = 'root';
 $password = '';
 
 try {
+    // WE USE PDO to connect to our database
+    // PDO represents a connection between PHP and a Database server
+    // In other words, defines a lightweight, consistent interface for accessing databases in PHP
+
     $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
